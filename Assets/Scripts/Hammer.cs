410fab78d2c1310059990213d16bf37b26cbee7e
Jbordier <jean-baptiste.bordier@irisa.fr>
2022-08-13 17:18:51 +0200

    First try of hammer

--- Assets/Scripts/Hammer.cs
+++ Assets/Scripts/Hammer.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class Hammer : MonoBehaviour
 {
-    public GameObject hammer;
-    public Quaternion initRotation;
+    private Vector3 GoalRotation;
+    public AnimationCurve curveDescente;
+    public AnimationCurve curveMontee;
+    AnimationCurve _curve;
+    public float speed = 0.5f;
+    bool flow = true;
+    private float _current, _target;
+    private Vector3 initialRotation;
     void Start()
     {
-        initRotation = hammer.transform.rotation;
+        GoalRotation = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z+90);
+        initialRotation = transform.rotation.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      if(_current == 0)
+        {
+            _target = 1;
+            _curve = curveDescente;
+        }
+        else if (_current == 1)
+        {
+            _target = 0;
+            _curve = curveMontee;
+        }
+        Debug.Log(_current);
+        _current = Mathf.MoveTowards(_current, _target, speed * Time.deltaTime);
+
+        transform.rotation = Quaternion.Lerp(Quaternion.Euler(initialRotation), Quaternion.Euler(GoalRotation), _curve.Evaluate(_current));
+
     }
 }
